f9d8086ddf0e9327ee66da88fc3f7cc84d0c899f
Najmul Huda <najmul.myself@gmail.com>
2021-01-31 02:05:22 +0600

    Phone Function

--- script.js
+++ script.js
@@ -11,36 +11,49 @@
 // });
 
 
-reduce('iphoneReduce','iphoneNumber');
-reduce('caseReduce','caseNumber');
+// reduce('iphoneReduce','iphoneNumber');
+// reduce('caseReduce','caseNumber');
 
-function reduce(id,iphoneOrCaseArea){
-    const reduceNum = document.getElementById(id);
-    reduceNum.addEventListener('click',function(){
-        const reduceValue = document.getElementById(iphoneOrCaseArea).value--;
-        if(reduceValue == 0){
-            document.getElementById(iphoneOrCaseArea).value = 0;
-        }
-        return reduceValue;
-    })
-    // return reduceValue;
-}
+// function reduce(id,iphoneOrCaseArea){
+//     const reduceNum = document.getElementById(id);
+//     reduceNum.addEventListener('click',function(){
+//         const reduceValue = document.getElementById(iphoneOrCaseArea).value--;
+//         if(reduceValue == 0){
+//             document.getElementById(iphoneOrCaseArea).value = 0;
+//         }
+//         return reduce
+//     })
+// }
 
-increse('iphoneIncrese','iphoneNumber');
-increse('caseIncrese','caseNumber');
+// increse('iphoneIncrese','iphoneNumber');
+// increse('caseIncrese','caseNumber');
 
-function increse(id,iphoneOrCaseArea){
-    const increseNum = document.getElementById(id);
-    increseNum.addEventListener('click', function(){
-    const increseValue = document.getElementById(iphoneOrCaseArea).value++;
+// function increse(id,iphoneOrCaseArea){
+//     const increseNum = document.getElementById(id);
+//     increseNum.addEventListener('click', function(){
+//     const increseValue = document.getElementById(iphoneOrCaseArea).value++;
     
-    return increseValue;
-})
-    let increseNewValue = increseValue + 0;
-    console.log(increseNewValue)
-}
-const casePrice = document.getElementById('currentCasePrice').innerText;
-const parseCasePrice = parseFloat(casePrice);
-const totalCasePrice = increseNewValue * parseCasePrice;
-document.getElementById('currentCasePrice').innerText = totalCasePrice;
+// })
+// }
+// const casePrice = document.getElementById('currentCasePrice').innerText;
+// const parseCasePrice = parseFloat(casePrice);
+// const totalCasePrice = increseNewValue * parseCasePrice;
+// document.getElementById('currentCasePrice').innerText = totalCasePrice;
 
+document.getElementById('iphoneReduce').addEventListener('click',function(){
+    const reduceNum = document.getElementById('iphoneNumber');
+    const parseNum = parseInt(reduceNum.value);
+    const caseCount = parseNum - 1;
+    reduceNum.value = caseCount;
+    const total = caseCount * 1219;
+    document.getElementById('totalPhonePrice').innerText = '$' + total;
+});
+
+document.getElementById('iphoneIncrese').addEventListener('click',function(){
+    const increseNum = document.getElementById('iphoneNumber');
+    const parseNum = parseInt(increseNum.value);
+    const caseCount = parseNum +1;
+    increseNum.value = caseCount;
+    const total = caseCount * 1219;
+    document.getElementById('totalPhonePrice').innerText = '$' + total;
+})
